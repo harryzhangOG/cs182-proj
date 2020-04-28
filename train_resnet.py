93385727768ce5ef381a977d6f5e41d3793256ec
Harry Zhang <harryzhang@Harrys-MacBook-Pro.local>
2020-04-28 15:09:29 -0700

    training code done

--- train_resnet.py
+++ train_resnet.py
@@ -1,4 +1,6 @@
 import torch
+import numbers
+import numpy as np
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,15 +9,80 @@ from tiny_dataset import TinyImageNet
 from torch.autograd import Variable
 from show_images import show_images_horizontally
 from resnet import BottleNeck, BaseBlock, ResNet
+from PIL import ImageFilter
+from PIL import Image
+import cv2
+
+# Helper class for adding Gaussian noises to images
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, image):
+        a = np.array(image)
+
+        noisy_image = np.zeros(a.shape, np.float32)
+        gaussian0 = np.random.normal(0, 10, (64, 64))
+        noisy_image[:, :, 0] = a[:, :, 0] + gaussian0
+        gaussian1 = np.random.normal(0, 10, (64, 64))
+        noisy_image[:, :, 1] = a[:, :, 1] + gaussian1
+        gaussian2 = np.random.normal(0, 10, (64, 64))
+        noisy_image[:, :, 2] = a[:, :, 2] + gaussian2
+
+        cv2.normalize(noisy_image, noisy_image, 0, 255, cv2.NORM_MINMAX, dtype=-1)
+        noisy_image = noisy_image.astype(np.uint8)
+
+        return Image.fromarray(noisy_image)
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+# Helper class for adding Gaussian filter (blur) to images
+class AddGaussianFilter(object):
+    def __init__(self, radius):
+        if isinstance(radius, numbers.Number):
+            self.min_radius = radius
+            self.max_radius = radius
+        elif isinstance(radius, list):
+            if len(radius) != 2:
+                raise Exception(
+                    "`radius` should be a number or a list of two numbers")
+            if radius[1] < radius[0]:
+                raise Exception(
+                    "radius[0] should be <= radius[1]")
+            self.min_radius = radius[0]
+            self.max_radius = radius[1]
+        else:
+            raise Exception(
+                "`radius` should be a number or a list of two numbers")
+
+    def __call__(self, image):
+        radius = np.random.uniform(self.min_radius, self.max_radius)
+        return image.filter(ImageFilter.GaussianBlur(radius)) 
+
 
 def train():
+    # Normalize the data. Not needed if we have batchnorm
     normalize = transforms.Normalize((.5, .5, .5), (.5, .5, .5))
 
+    """
+    Data Augmentation:
+        Randomly apply the following actions to images in each epoch:
+            1. Random Horizontal Flip
+            2. Random Vertical Flip
+            3. Random Affine transformation
+            4. Random crop
+            5. Add Gaussian noise
+            6. Add Blurring
+    """
     augmentation = transforms.RandomApply([
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
         transforms.RandomAffine(10, translate=(0.1, 0.2), scale=(0.8, 1.1), shear=3),
-        transforms.RandomResizedCrop(64)], p=.9)
+        transforms.RandomResizedCrop(64),
+        AddGaussianNoise(0., 1.),
+        AddGaussianFilter([0, 1])], p=.7)
 
     training_transform = transforms.Compose([
         transforms.Lambda(lambda x: x.convert("RGB")),
@@ -28,6 +95,7 @@ def train():
         transforms.ToTensor(),
         normalize])
 
+    # EXTRACT THE DATASET FROM OUR CUSTOM DATASET
     root = 'data/tiny-imagenet-200'
     training_set = TinyImageNet(root, 'train', transform=training_transform, in_memory=False)
     training_set = torch.utils.data.DataLoader(training_set, batch_size=128)
@@ -122,11 +190,17 @@ def train():
             }, 'resnetlight_rotate_epoch' + str(epoch + 1) + '.pth')
         print('model saved')
 
+    return trainLoss, valLoss, trainAcc, valAcc, total_loss
+
 
 
 
 
 
 if __name__ == "__main__":
-    train()
+    trainLoss, valLoss, trainAcc, valAcc, total_loss = train()
+    np.save('trainLoss.npy', trainLoss)
+    np.save('valLoss.npy', valLoss)
+    np.save('trainAcc.npy', trainAcc)
+    np.save('valAcc.npy', valAcc)
 
